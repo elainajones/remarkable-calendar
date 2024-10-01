8f8030ed2022948c955000a1490da9f277db574a
Kevin Carrion <86639616+kevinzl13@users.noreply.github.com>
2024-10-01 18:20:28 -0500

    feat(colors): add colors.py to change palette more easily
    A new file `colors.py` has been added with a warm color palette. The old colors are commented out.

---
+++ colors.py
@@ -0,0 +1,29 @@
+# DRAW_COLOR = (26, 26, 26)
+# TEXT_COLOR = (26, 26, 26)
+# WEEKEND_SHADING = (230, 230, 230)
+# GRID_LINE_COLOR = (179, 179, 179)
+# NOON_MARKER_TEXT_COLOR = (26, 26, 26)
+# NOON_MARKER_FILL_COLOR = (255, 255, 255)
+# NOON_MARKER_BORDER_COLOR = (255, 255, 255)
+# LEADING_DATE_COLOR = (179, 179, 179)
+
+DRAW_COLOR = (255, 99, 71)  # Tomato
+TEXT_COLOR = (255, 165, 0)  # Orange
+WEEKEND_SHADING = (255, 228, 196)  # Bisque
+GRID_LINE_COLOR = (255, 215, 0)  # Gold
+NOON_MARKER_TEXT_COLOR = (255, 69, 0)  # Red-Orange
+NOON_MARKER_FILL_COLOR = (255, 255, 224)  # Light Yellow
+NOON_MARKER_BORDER_COLOR = (255, 140, 0)  # Dark Orange
+LEADING_DATE_COLOR = (255, 182, 193)  # Light Pink
+
+
+colors = {
+    "draw_color": DRAW_COLOR,
+    "text_color": TEXT_COLOR,
+    "weekend_shading": WEEKEND_SHADING,
+    "grid_line_color": GRID_LINE_COLOR,
+    "noon_marker_text_color": NOON_MARKER_TEXT_COLOR,
+    "noon_marker_fill_color": NOON_MARKER_FILL_COLOR,
+    "noon_marker_border_color": NOON_MARKER_BORDER_COLOR,
+    "leading_date_color": LEADING_DATE_COLOR,
+}
