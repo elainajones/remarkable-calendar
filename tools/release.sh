01b7305467dc9b4289e08e1c44b74b1c881f09cb
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-08-13 12:05:24 -0700

    feat: Create standalone executables
    Uses pyinstaller to create standalone executables for creating
    calendar templates

--- tools/release.sh
+++ tools/release.sh
@@ -2,13 +2,14 @@
 
 main() {
     declare year_today=$(date +%Y);
+    mkdir build 2> /dev/null;
 
     for i in $(seq 0 4); do
         declare args=( \
             "--start-date \"$((year_today+i))/01/01\" " \
             "--end-date \"$((year_today+i+1))/02/01\" " \
             "--hour-interval 6" \
-            "--out $((year_today+i))_calendar.pdf"
+            "--out build/$((year_today+i))_calendar.pdf"
         );
         python main.py ${args[@]};
     done
@@ -19,9 +20,20 @@ main() {
             "--end-date \"$((year_today+i+1))/02/01\" " \
             "--week-start sunday " \
             "--hour-interval 6" \
-            "--out $((year_today+i))_sunday_start_calendar.pdf"
+            "--out build/$((year_today+i))_sunday_start_calendar.pdf"
         );
         python main.py ${args[@]};
     done
+
+	pyinstaller \
+		--noconfirm \
+		--onefile \
+		--clean \
+		--specpath .pyinstaller/spec \
+		--distpath ./build \
+		--workpath .pyinstaller/build \
+		--name calendar-creator-linux64.bin \
+		--add-data "$(realpath res/GentiumPlus-6.200/GentiumPlus-Regular.ttf):res/GentiumPlus-6.200/" \
+		main.py
 }
 main;
