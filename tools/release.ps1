41d954a5c1f55f0c264224311f164239ecd22433
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-08-31 18:50:41 -0700

    fix: Add missing ./build directory in release.ps1

--- tools/release.ps1
+++ tools/release.ps1
@@ -1,30 +1,36 @@
-$path = (Resolve-Path res/GentiumPlus-6.200/GentiumPlus-Regular.ttf).Path
-$year = (date).Year
+function main {
+    $path = (Resolve-Path res/GentiumPlus-6.200/GentiumPlus-Regular.ttf).Path
+    $year = (date).Year
 
-for ($i = 1; $i -le 4; $i++) {
-    python main.py `
-        --start-date "$($year + $i)/01/01" `
-        --end-date "$($year + $i + 1)/01/01" `
-        --hour-interval 6 `
-        --out "build/$($year + $i)_calendar.pdf"
+    New-Item -ItemType Directory -Path build -Force > $null
+
+    for ($i = 1; $i -le 4; $i++) {
+        python main.py `
+            --start-date "$($year + $i)/01/01" `
+            --end-date "$($year + $i + 1)/01/01" `
+            --hour-interval 6 `
+            --out "build/$($year + $i)_calendar.pdf"
+    }
+
+    for ($i = 1; $i -le 4; $i++) {
+        python main.py `
+            --start-date "$($year + $i)/01/01" `
+            --end-date "$($year + $i + 1)/01/01" `
+            --week-start sunday `
+            --hour-interval 6 `
+            --out "build/$($year + $i)_sunday_start_calendar.pdf"
+    }
+
+    pyinstaller `
+        --noconfirm `
+        --onefile `
+        --clean `
+        --specpath .pyinstaller/spec `
+        --distpath ./build `
+        --workpath .pyinstaller/build `
+        --name calendar-creator-win64 `
+        --add-data "${path}:./res/GentiumPlus-6.200/" `
+        main.py
 }
+main
 
-for ($i = 1; $i -le 4; $i++) {
-    python main.py `
-        --start-date "$($year + $i)/01/01" `
-        --end-date "$($year + $i + 1)/01/01" `
-        --week-start sunday `
-        --hour-interval 6 `
-        --out "build/$($year + $i)_sunday_start_calendar.pdf"
-}
-
-pyinstaller `
-    --noconfirm `
-    --onefile `
-    --clean `
-    --specpath .pyinstaller/spec `
-    --distpath ./build `
-    --workpath .pyinstaller/build `
-    --name calendar-creator-win64 `
-    --add-data "${path}:./res/GentiumPlus-6.200/" `
-    main.py
